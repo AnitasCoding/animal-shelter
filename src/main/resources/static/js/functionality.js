8ac60092fbca5b169b1b54a20b0d0fdded3157bc
Anita <1989anitagion@gmail.com>
2020-10-19 09:46:53 +0200

    burger-menu + responsive css

--- src/main/resources/static/js/functionality.js
+++ src/main/resources/static/js/functionality.js
@@ -44,3 +44,19 @@ function cancel_login() {
 function changeURLPath(path) {
     window.location.pathname = path;
 }
+
+function setBurgerMenu() {
+    if (window.innerWidth <= 755 && document.getElementById("menu") === null) {
+        let text = document.getElementsByClassName("club_name").item(0);
+        let span = text.children.item(0);
+        let menu = document.createElement("div");
+        menu.setAttribute("id", "menu");
+        let menu_burger = document.createElement("div");
+        menu_burger.setAttribute("id", "menu_burger");
+        menu.appendChild(menu_burger);
+        text.insertBefore(menu, span);
+    }
+    if (window.innerWidth > 755 && document.getElementById("menu") !== null) {
+        document.getElementById("menu").remove();
+    }
+}
